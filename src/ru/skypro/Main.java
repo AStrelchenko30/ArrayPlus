a59f99491264efd8e4b927f5c6311440d9d769fc
VIKI <anystrelchenko30@gmail.com>
2022-04-29 13:21:44 +0300

    Доп задание по теме "Циклы"

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,28 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Доп.Задание 4");
+        for (int i = 1; i <= 20; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + ":ping pong");
+            } else if (i % 5 == 0) {
+                System.out.println(i + ":pong");
+            } else if (i % 3 == 0) {
+                System.out.println(i + ":ping");
+            } else {
+                System.out.println(i);
+            }
+        }
+        System.out.println("Доп.задание 5");
+        int a = 0;
+        int b = 1;
+        int c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 0; i < 8; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
     }
 }
